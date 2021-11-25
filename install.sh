1c993f54a2b2b80f0f33b7019445aff308a0b616
Marcelo <marcelo.siqueira99@outlook.com>
2021-11-25 18:26:32 -0300

    check if should swap, cleanup and beginning of complete install

--- install.sh
+++ install.sh
@@ -22,9 +22,8 @@ function get_password() {
     echo $password
 }
 
-# $install_disk
 function final_warning() {
-    doit=$(yes_no_box "Final Warning!" "You are about to write changes to the Wdisk.
+    doit=$(yes_no_box "Final Warning!" "You are about to write changes to the disk.
 THIS ACTION CANNOT BE UNDONE!
 Let's do it?")
     if [ $doit != "True" ]; then
@@ -34,12 +33,25 @@ Let's do it?")
 }
 
 message_box "Welcome!" "Welcome to monarchy-installer! Yet another arch installer.
+
 WARNING: This script is still in development and might brick your entire system! Use it only inside a VM or in a spare computer!
+
+If you misstype or selected the wrong option, select no on the 'Final Warning' box and start from the beginning. This will be fixed in the future.
 "
 
 awser=$(yes_no_box "Monarchy" "Shall we begin?")
-# in the future give an option of a minimal arch install or full monarchy installation
+
 if [ $awser = "True" ]; then
+
+    # ==================== Type ====================
+    message_box "Installation Type" "Monarchy-installer has 2 installation type, minimal (wip, missing encryption) and complete (wip).
+
+Minimal: Minimal arch install and some essential utilities.
+Complete: Cinnamon desktop environment and a lot, a lot more :)
+"
+
+    types=("Minimal" "Complete")
+    install_type=$(menu_box "Installation Type" "Which one would you like to install?" "" ${types[@]})
 
     # ==================== Location ====================
 
@@ -64,9 +76,7 @@ if [ $awser = "True" ]; then
     disks=$(get_disks)
     install_disk=$(menu_box "Disks" "Select which drive to install Arch Linux." "True" ${disks[@]})
     should_swap=$(yes_no_box "Swap" "Would you like to create a 2G Swap partition?")
-    if [ $should_swap = "True" ]; then
-        echo "Swap!"
-    fi
+
     should_encrypt=$(yes_no_box "Disk encryption" "Would you like to encrypt your new installation?")
     if [ $should_encrypt = "True" ]; then
         encrypt_password=$(get_password "Encryption")
@@ -80,32 +90,32 @@ if [ $awser = "True" ]; then
 
     # ==================== Install ====================
 
-    final_warning $install_disk
+    final_warning
+
     # == 1 ==
+    
     update_system_clock
-    disk_partition $install_disk $should_swap
-    format_partition $install_disk $should_swap $should_encrypt
-    mount_partition $install_disk
-
-    # == 2 ==
+    disk_partition 
+    format_partition 
+    mount_partition 
     install_base
-
-    # == 3 ==
     gen_fstab
 
+    # == 2 ==
+
     mkdir /mnt/post-chroot-temp/
-    cp /root/monarchy-installer/post-chroot.sh /mnt/post-chroot-temp/
-    arch-chroot /mnt /usr/bin/bash /post-chroot-temp/post-chroot.sh $timezone $locale_select $pc_name $password $user_name $keymap_select
+    cp /root/monarchy-installer/post-chroot-minimal.sh /mnt/post-chroot-temp/
+    arch-chroot /mnt /usr/bin/bash /post-chroot-temp/post-chroot-minimal.sh $timezone $locale_select $pc_name $password $user_name $keymap_select
 
-
+    if [ $install_type = "Complete" ]; then
+        cp /root/monarchy-installer/post-chroot-complete.sh /mnt/post-chroot-temp/
+        arch-chroot /mnt /usr/bin/bash /post-chroot-temp/post-chroot-complete.sh
+    fi
 
     rm -r /mnt/post-chroot-temp/
     umount -R /mnt
 
     message_box "Done!" "Installation finished! You can now reboot and login into your new system!"
-
-    # == 4 ==
-    #profit
 
     # ==================== Finish ====================
 
